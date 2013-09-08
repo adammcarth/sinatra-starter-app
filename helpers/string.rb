c11085f88962ff3958bfeea5f870c8d1ba7a3ec1
Adam McArthur <adam@adammcarthur.net>
2013-09-08 20:27:50 +1000

    Add unindent helper to string class

--- helpers/string.rb
+++ helpers/string.rb
@@ -3,6 +3,9 @@ class String
     num = (" " * spaces)
     gsub(/^/, num)
   end
+  def unindent 
+    gsub(/^#{scan(/^\s*/).min_by{|l|l.length}}/, "")
+  end
   def remove_all_whitespaces
     gsub(/\s+/, "")
   end
